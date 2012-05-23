abefb9fd1f2c894b7ec8b8cc0ac20d6a3f1f4218
ragtek <ragga@ragtek.org>
2012-05-23 08:05:52 +0200

    added issue status design

--- js/Ragtek.js
+++ js/Ragtek.js
@@ -7,8 +7,14 @@ $.each(repos, function (i, v) {
     var repo = user.repo(v);
     issues = repo.issues('open', function (i) {
         $.each(i, function (i, issue) {
-            console.log(issue);
-            $elem = $( '<li>[' + issue['labels'][0] + '] <a href="' + issue['html_url'] + '" rel="popover" title="' +issue['body'] + '">'   + issue['title'] + '</a> ( ' + issue['repo']['name'] +')</li>');
+
+            if (issue['labels'][0] == 'bug'){
+                $prefix ='<span class="label label-important">Bug</span>';
+            }
+            else {
+                $prefix =  '<span class="label label-info" >' + issue['labels'][0] + '</span>';
+            }
+            $elem = $( '<li>' + $prefix + ' <a href="' + issue['html_url'] + '" rel="popover" title="' +issue['body'] + '">'   + issue['title'] + '</a> ( ' + issue['repo']['name'] +')</li>');
 
             $issuesContainer.append($elem);
         })
@@ -20,3 +26,4 @@ $issuesContainer.html(issuesForOutPut);
 $('#issues').popover({
     selector: "a[rel=popover]",placement: "top"
 })
+
