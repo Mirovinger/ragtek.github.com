d831174de068a4756d0127c2ee1950fc0b3cc286
ragtek <ragga@ragtek.org>
2012-05-25 21:31:31 +0200

    no message

--- js/Ragtek.js
+++ js/Ragtek.js
@@ -1,12 +1,10 @@
 var user = new GitHub.User("ragtek");
-
 $issuesContainer = $('#placeholder');
 
 $.each(repos, function (i, v) {
     var repo = user.repo(v);
     issues = repo.issues('open', function (i) {
         $.each(i, function (i, issue) {
-
             if (issue['labels'][0] == 'bug'){
                 $prefix ='<span class="label label-important">Bug</span>';
             }
@@ -14,11 +12,9 @@ $.each(repos, function (i, v) {
                 $prefix =  '<span class="label label-info" >' + issue['labels'][0] + '</span>';
             }
             $elem = $( '<tr><td>' + $prefix + '</td><td> &nbsp;<a href="' + issue['html_url'] + '" rel="popover" title="' +issue['body'] + '">'   + issue['title'] + '</a> ( ' + issue['repo']['name'] +')</td></tr>');
-
             $issuesContainer.append($elem);
         })
     });
-
 })
 
 $('#issues').popover({
