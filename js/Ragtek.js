53896c9144118df9b75572cb741fe5a7fbad7a4c
ragtek <ragga@ragtek.org>
2012-05-25 13:57:20 +0200

    index page optimization

--- js/Ragtek.js
+++ js/Ragtek.js
@@ -1,5 +1,4 @@
 var user = new GitHub.User("ragtek");
-var issuesForOutPut = {};
 
 $issuesContainer = $('#placeholder');
 
@@ -14,7 +13,7 @@ $.each(repos, function (i, v) {
             else {
                 $prefix =  '<span class="label label-info" >' + issue['labels'][0] + '</span>';
             }
-            $elem = $( '<li>' + $prefix + ' <a href="' + issue['html_url'] + '" rel="popover" title="' +issue['body'] + '">'   + issue['title'] + '</a> ( ' + issue['repo']['name'] +')</li>');
+            $elem = $( '<tr><td>' + $prefix + '</td><td> &nbsp;<a href="' + issue['html_url'] + '" rel="popover" title="' +issue['body'] + '">'   + issue['title'] + '</a> ( ' + issue['repo']['name'] +')</td></tr>');
 
             $issuesContainer.append($elem);
         })
@@ -23,6 +22,6 @@ $.each(repos, function (i, v) {
 })
 
 $('#issues').popover({
-    selector: "a[rel=popover]",placement: "top"
+    selector: "a[rel=popover]",placement: "right"
 })
 
